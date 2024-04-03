637996f9326162a75b7598ff086a619abc4b8e64
Rpyung23 <guaman1579@gmail.com>
2024-04-03 00:14:14 -0500

    update 2

--- database/config.ts
+++ database/config.ts
@@ -1,4 +1,4 @@
-export const DB_HOST = process.env.DB_HOST || '159.223.111.107:3306'
+export const DB_HOST = process.env.DB_HOST || '159.223.111.107'
 export const DB_NAME = process.env.DB_NAME || 'dbmanager'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASS = process.env.DB_PASS || 'Pum@15001232023*'
