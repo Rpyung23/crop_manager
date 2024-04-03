b8be5f87c0f7b08d10ef8471b9d82df465dafefa
Rpyung23 <guaman1579@gmail.com>
2024-04-02 23:55:34 -0500

    update

--- dist/database/config.js
+++ dist/database/config.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB_PASS = exports.DB_USER = exports.DB_NAME = exports.DB_HOST = void 0;
-exports.DB_HOST = process.env.DB_HOST || '159.223.111.107';
+exports.DB_HOST = process.env.DB_HOST || '159.223.111.107:3306';
 exports.DB_NAME = process.env.DB_NAME || 'dbmanager';
 exports.DB_USER = process.env.DB_USER || 'root';
 exports.DB_PASS = process.env.DB_PASS || 'Pum@15001232023*';
